66a91d5bcda22a37d27ae0b76fcebe64c2fc8b44
Nelson Chung <chihchun.chung@gmail.com>
2023-07-23 18:21:07 +0800

    新增以下功能 1. 座位表 2. 結帳，日結，月結功能

--- lib/boss_mode.dart
+++ lib/boss_mode.dart
@@ -72,6 +72,22 @@ class _BossModeScreenState extends State<BossModeScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // 座位分配圖
+            const Text(
+              '座位分配圖',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SeatCard(seatNumber: '1', capacity: 4),
+                SeatCard(seatNumber: '2', capacity: 2),
+                SeatCard(seatNumber: '3', capacity: 2),
+                SeatCard(seatNumber: '4', capacity: 2),
+              ],
+            ),
+            const SizedBox(height: 20),
             // 菜單
             const Text(
               '菜單',
@@ -106,7 +122,33 @@ class _BossModeScreenState extends State<BossModeScreen> {
                 },
               ),
             ),
-            // ...
+            // 結帳按鈕、日結按鈕和月結按鈕
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // 結帳按鈕的邏輯
+                    // ...
+                  },
+                  child: const Text('結帳'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // 日結按鈕的邏輯
+                    // ...
+                  },
+                  child: const Text('日結'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // 月結按鈕的邏輯
+                    // ...
+                  },
+                  child: const Text('月結'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -132,4 +174,31 @@ class MenuItem {
     required this.name,
     required this.imagePath,
   });
+}
+
+class SeatCard extends StatelessWidget {
+  final String seatNumber;
+  final int capacity;
+
+  const SeatCard({super.key, required this.seatNumber, required this.capacity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '座位 $seatNumber',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text('$capacity 人'),
+          ],
+        ),
+      ),
+    );
+  }
 }
