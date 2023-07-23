8ac45920d130eee678613f741766be629238d46a
Nelson Chung <chihchun.chung@gmail.com>
2023-07-24 00:24:32 +0800

    嘗試調整背景圖與背景顏色

--- lib/main.dart
+++ lib/main.dart
@@ -26,32 +26,44 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('義麵房'),
+        backgroundColor: const Color(0xFF3FC979),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const CustomerOrderScreen()),
-                );
-              },
-              child: const Text('顧客模式'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const BossModeScreen()),
-                );
-              },
-              child: const Text('老闆模式'),
-            ),
-          ],
+      body: Container(
+        color: const Color(0xFF3FC979), // 背景顏色
+        /*
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'), // 背景圖片
+            fit: BoxFit.cover,
+          ),
+        ),
+        */
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CustomerOrderScreen()),
+                  );
+                },
+                child: const Text('顧客模式'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BossModeScreen()),
+                  );
+                },
+                child: const Text('老闆模式'),
+              ),
+            ],
+          ),
         ),
       ),
     );
